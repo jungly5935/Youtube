29bf2f5e8a0fbc03e558e6e62f8397e5e61228bc
sk <1724271@donga.ac.kr>
2021-04-27 22:31:39 +0900

    ADD moreBtn_list

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
 const moreBtn = document.querySelector('.info .metadata .moreBtn');
 const title = document.querySelector('.info .metadata .title');
+const actionBtn = document.querySelectorAll('.info .actions button');
+const subscribe = document.querySelector('.info .channel .subscribe')
+const upNextmoreBtn = document.querySelector('.upNext .item .moreBtn');
+const upNextlist = document.querySelector('.upNext .item .moreBtn_list')
 
 moreBtn.addEventListener('click', ()=>{
     moreBtn.classList.toggle('clicked');
     title.classList.toggle('active');
+})
+
+for(let i=0; i<actionBtn.length;i++){
+actionBtn[i].addEventListener('click', ()=> {
+    actionBtn[i].classList.toggle('active');
+})
+}
+
+subscribe.addEventListener('click', () => {
+
+    if (subscribe.innerHTML==='SUBSCRIBE')   { 
+        subscribe.innerHTML = '구독중';
+        subscribe.style.color="gray";
+    }
+    else  {
+        subscribe.innerHTML = 'SUBSCRIBE'
+        subscribe.style.color="red";
+   
+    }  
+})
+
+upNextmoreBtn.addEventListener('click', ()=> {
+    // 작은 서브메뉴 나타나게 하기
+    upNextlist.classList.toggle('active');
+
 })
